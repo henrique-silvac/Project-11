7a2e0e964cc31efa347c37390ed974a0663018cc
Henrique Silva de Carvalho <henrique.silvac@outlook.com>
2021-07-29 12:59:02 -0300

    Wrap Up

--- Project 11/GameScene.swift
+++ Project 11/GameScene.swift
@@ -8,25 +8,19 @@
 import SpriteKit
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
-    var scoreLabel: SKLabelNode!
     
-    var score = 0 {
+    let ballColors = ["Blue", "Cyan", "Green", "Grey", "Purple", "Red", "Yellow"]
+    
+    var remainingBallsLabel: SKLabelNode!
+    
+    var remainingBalls = 5 {
         didSet {
-            scoreLabel.text = "Score: \(score)"
+            remainingBallsLabel.text = "Balls: \(remainingBalls)"
         }
     }
     
-    var editLabel: SKLabelNode!
-    
-    var editingMode: Bool = false {
-        didSet {
-            if editingMode {
-                editLabel.text = "Done"
-            } else {
-                editLabel.text = "Edit"
-            }
-        }
-    }
+    var newGameLabel: SKLabelNode!
+    var resultLabel: SKLabelNode!
     
     override func didMove(to view: SKView) {
         let background = SKSpriteNode(imageNamed: "background")
@@ -35,64 +29,67 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         background.zPosition = -1
         addChild(background)
         
-        scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
-        scoreLabel.text = "Score: 0"
-        scoreLabel.horizontalAlignmentMode = .right
-        scoreLabel.position = CGPoint(x: 980, y: 700)
-        addChild(scoreLabel)
+        remainingBallsLabel = SKLabelNode(fontNamed: "Chalkduster")
+        remainingBallsLabel.text = "Balls: \(remainingBalls)"
+        remainingBallsLabel.horizontalAlignmentMode = .right
+        remainingBallsLabel.position = CGPoint(x: 980, y: 700)
+        addChild(remainingBallsLabel)
         
-        editLabel = SKLabelNode(fontNamed: "Chalkduster")
-        editLabel.text = "Edit"
-        editLabel.position = CGPoint(x: 80, y: 700)
-        addChild(editLabel)
+        newGameLabel = SKLabelNode(fontNamed: "Chalkduster")
+        newGameLabel.text = "New game"
+        newGameLabel.position = CGPoint(x: 130, y: 700)
+        addChild(newGameLabel)
+        
+        resultLabel = SKLabelNode(fontNamed: "Chalkduster")
+        resultLabel.text = ""
+        resultLabel.horizontalAlignmentMode = .center
+        resultLabel.position = CGPoint(x: 512, y: 700)
+        addChild(resultLabel)
+        
         
         physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
         physicsWorld.contactDelegate = self
-        
-        makeSlot(at: CGPoint(x: 128, y: 0), isGood: true)
-        makeSlot(at: CGPoint(x: 384, y: 0), isGood: false)
-        makeSlot(at: CGPoint(x: 640, y: 0), isGood: true)
-        makeSlot(at: CGPoint(x: 896, y: 0), isGood: false)
         
         makeBouncer(at: CGPoint(x: 0, y: 0))
         makeBouncer(at: CGPoint(x: 256, y: 0))
         makeBouncer(at: CGPoint(x: 512, y: 0))
         makeBouncer(at: CGPoint(x: 768, y: 0))
         makeBouncer(at: CGPoint(x: 1024, y: 0))
+        
+        makeSlot(at: CGPoint(x: 128, y: 0), isGood: true)
+        makeSlot(at: CGPoint(x: 384, y: 0), isGood: false)
+        makeSlot(at: CGPoint(x: 640, y: 0), isGood: true)
+        makeSlot(at: CGPoint(x: 896, y: 0), isGood: false)
+        
+        newGame()
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        guard let touch = touches.first else { return }
+        guard let touch = touches.first else {
+            return
+        }
+        
         let location = touch.location(in: self)
         let objects = nodes(at: location)
         
-        if objects.contains(editLabel) {
-            editingMode.toggle()
-        } else {
-            if editingMode {
-                let size = CGSize(width: Int.random(in: 16...128), height: 16)
-                let box = SKSpriteNode(color: UIColor(red: CGFloat.random(in: 0...1), green: CGFloat.random(in: 0...1), blue: CGFloat.random(in: 0...1), alpha: 1), size: size)
-                box.zRotation = CGFloat.random(in: 0...3)
-                box.position = location
-                box.physicsBody = SKPhysicsBody(rectangleOf: box.size)
-                box.physicsBody?.isDynamic = false
-                addChild(box)
-            } else {
-                let ball = SKSpriteNode(imageNamed: "ballRed")
-                ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width/2.0)
-                ball.physicsBody?.restitution = 0.4
-                ball.physicsBody?.contactTestBitMask = ball.physicsBody?.collisionBitMask ?? 0
-                ball.position = location
-                ball.name = "ball"
-                addChild(ball)
-            }
+        if objects.contains(newGameLabel) {
+            newGame()
+        }
+        else if remainingBalls > 0 && !isBallInPlay() {
+            let ball = SKSpriteNode(imageNamed: "ball\(ballColors.randomElement()!)")
+            ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.0)
+            ball.physicsBody?.contactTestBitMask = ball.physicsBody?.collisionBitMask ?? 0
+            ball.physicsBody?.restitution = 0.4
+            ball.position = CGPoint(x: location.x, y: 700)
+            ball.name = "ball"
+            addChild(ball)
         }
     }
     
     func makeBouncer(at position: CGPoint) {
         let bouncer = SKSpriteNode(imageNamed: "bouncer")
         bouncer.position = position
-        bouncer.physicsBody = SKPhysicsBody(circleOfRadius: bouncer.size.width/2)
+        bouncer.physicsBody = SKPhysicsBody(circleOfRadius: bouncer.size.width / 2.0)
         bouncer.physicsBody?.isDynamic = false
         addChild(bouncer)
     }
@@ -105,7 +102,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             slotBase = SKSpriteNode(imageNamed: "slotBaseGood")
             slotGlow = SKSpriteNode(imageNamed: "slotGlowGood")
             slotBase.name = "good"
-        } else {
+        }
+        else {
             slotBase = SKSpriteNode(imageNamed: "slotBaseBad")
             slotGlow = SKSpriteNode(imageNamed: "slotGlowBad")
             slotBase.name = "bad"
@@ -114,7 +112,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         slotBase.position = position
         slotGlow.position = position
         
-        slotBase.physicsBody = SKPhysicsBody(rectangleOf:  slotBase.size)
+        slotBase.physicsBody = SKPhysicsBody(rectangleOf: slotBase.size)
         slotBase.physicsBody?.isDynamic = false
         
         addChild(slotBase)
@@ -125,19 +123,41 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         slotGlow.run(spinForever)
     }
     
-    func collision(between ball: SKNode, object: SKNode) {
+    func collisionBetween(ball: SKNode, object: SKNode) {
+        if object.name == "box" {
+            object.removeFromParent()
+        }
         if object.name == "good" {
-            destroy(ball: ball)
-            score += 1
-        } else if object.name == "bad" {
-            destroy(ball: ball)
-            score -= 1
+            destroy(ball: ball, isGood: true)
+            manageResult()
+        }
+        else if object.name == "bad" {
+            destroy(ball: ball, isGood: false)
+            remainingBalls -= 1
+            manageResult()
         }
     }
     
-    func destroy(ball: SKNode) {
+    func manageResult() {
+        if !isRemainingBoxes() {
+            resultLabel.fontColor = UIColor.green
+            resultLabel.text = "VICTORY"
+        }
+        else if remainingBalls == 0 {
+            resultLabel.fontColor = UIColor.red
+            resultLabel.text = "DEFEAT"
+        }
+    }
+    
+    func destroy(ball: SKNode, isGood: Bool) {
         if let fireParticles = SKEmitterNode(fileNamed: "FireParticles") {
             fireParticles.position = ball.position
+            if isGood {
+                fireParticles.particleColorGreenRange = 1
+            }
+            else {
+                fireParticles.particleColorRedRange = 1
+            }
             addChild(fireParticles)
         }
         
@@ -149,9 +169,99 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         guard let nodeB = contact.bodyB.node else { return }
         
         if nodeA.name == "ball" {
-            collision(between: nodeA, object: nodeB)
-        } else if nodeB.name == "ball" {
-            collision(between: nodeB, object: nodeA)
+            collisionBetween(ball: nodeA, object: nodeB)
+        }
+        else if nodeB.name == "ball" {
+            collisionBetween(ball: nodeB, object: nodeA)
         }
     }
+    
+    func isBallInPlay() -> Bool {
+        for node in self.children {
+            if node.name == "ball" {
+                return true
+            }
+        }
+        return false
+    }
+    
+    func isRemainingBoxes() -> Bool {
+        for node in self.children {
+            if node.name == "box" {
+                return true
+            }
+        }
+        return false
+    }
+    
+    func newGame() {
+        remainingBalls = 5
+        
+        resultLabel.text = ""
+        
+        // remove remaining boxes and balls
+        for node in self.children {
+            if node.name == "box" || node.name == "ball" {
+                node.removeFromParent()
+            }
+        }
+        
+        makeRandomBoxes(number: 15)
+    }
+    
+    func makeRandomBoxes(number: Int) {
+        for _ in 1...number {
+            let size = CGSize(width: Int.random(in: 16...128), height: 16)
+            let color = getBoxColor()
+            let rotation = CGFloat.random(in: 0...3)
+            let position = CGPoint(x: CGFloat.random(in: 128...896), y: CGFloat.random(in: 200...568))
+            
+            let box = SKShapeNode(rectOf: size, cornerRadius: 3)
+            box.fillColor = color
+            box.strokeColor = color
+            box.lineWidth = 1
+            box.zRotation = rotation
+            box.position = position
+            box.physicsBody = SKPhysicsBody(rectangleOf: size)
+            box.physicsBody?.isDynamic = false
+            box.name = "box"
+            addChild(box)
+        }
+    }
+}
+
+func getBoxColor() -> UIColor {
+    
+    let colors = [
+        UIColor.red,
+        UIColor.magenta,
+        UIColor.blue,
+        UIColor.cyan,
+        UIColor.green,
+        UIColor.yellow,
+        UIColor.orange,
+        UIColor.purple,
+        UIColor.white
+    ]
+    
+    return colors.randomElement()!.withAlphaComponent(0.75)
+}
+
+func getBorderColor(boxColor: UIColor) -> UIColor {
+    if isDarkColor(color: boxColor) {
+        return UIColor(red: 1, green: 1, blue: 1, alpha: 0.4)
+    }
+    
+    return UIColor(red: 0, green: 0, blue: 0, alpha: 0.5)
+}
+
+func isDarkColor(color: UIColor) -> Bool {
+    
+    if let components = color.cgColor.converted(to: CGColorSpaceCreateDeviceRGB(), intent: .defaultIntent, options: nil)?.components {
+        let brightness = (components[0] * 299 + components[1] * 587 + components[2] * 114) / 1000
+        if brightness < 0.5 {
+            return true
+        }
+    }
+    return false
 }
